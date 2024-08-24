d5e03cf00b56e9174d89081d55affc740bf43531
changgenglu <eatbreakfast2012@gmail.com>
2024-08-24 14:36:19 +0800

    feat: first commit
    1. 開啟使用者回饋 modal 按鈕
    2. 使用者回饋 modal-card 關閉按鈕
    3. modal-card select, input, file, submit
    4. 按下 submit 後，顯示提交成功
    5. 提交成功 modal 五秒後關閉，或按下關閉難扭關閉

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,108 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="feedback-btn">
+    <button class="btn btn-info" @click="showModal(true)">send feedback</button>
+    <UserFeedback class="feedback-modal" :class="{ 'show-modal': modelType }" @close-modal="onCloseModal"></UserFeedback>
+    <!-- Success Modal -->
+    <div v-if="showSuccessModal" class="success-modal card">
+      <div class="card-body">
+        <p class="fs-2 text-success">提交成功</p>
+        <a href="" class="close-btn" @click="closeSuccessModal(false)">X</a>
+        <p>感謝您的反饋，我們將會繼續努力，提供更優質的服務！</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import UserFeedback from './UserFeedback.vue';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    UserFeedback,
+  },
+  data() {
+    return {
+      modelType: false,
+      showSuccessModal: false,
+    }
+  },
+  methods: {
+    showModal(status) {
+      this.modelType = status;  // Toggle the model state between true and false.
+    },
+    onCloseModal(state) {
+      this.showSuccessModal = true;
+
+      setTimeout(() => {
+        this.showSuccessModal = false;
+        this.modelType = state;  // Close the modal when the close button is clicked.
+      }, 5000);
+    },
+    closeSuccessModal(state) {
+      this.showSuccessModal = false;
+      this.modelType = state;
+    },
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+.feedback-btn {
+  margin-bottom: 20px;
+}
+
+.feedback-modal {
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+}
+
+.show-modal {
+  display: block;
+}
+
+.success-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 20px;
+  border: 1px solid #000;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+}
+
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 24px;
+  cursor: pointer;
+  color: #000;
 }
 </style>
---
+++ src/components/UserFeedback.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <div class="feedback-card card">
+            <div class="card-header">
+                <a href="" class="close-btn" @click="closeModal(false)">X</a>
+                <h3>User Feedback</h3>
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="type" class="">
+                        <span class="is-required">*</span>
+                        類別
+                    </label>
+                    <!-- <input type="text" class="form-control" id="type" placeholder="請選擇類別"> -->
+
+                    <select class="form-select" aria-label="Type select">
+                        <option selected> </option>
+                        <option value="1">操作問題</option>
+                        <option value="2">優化建議</option>
+                        <option value="3">Bug反饋</option>
+                        <option value="4">其他</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="title" class="">
+                        <span class="is-required">*</span>
+                        標題
+                        <small>(限30字內)</small>
+                    </label>
+                    <input type="text" class="form-control" id="title" placeholder="請輸入標題">
+                </div>
+                <div class="mb-3">
+                    <label for="describe" class="">
+                        <span class="is-required">*</span>
+                        描述
+                        <small>(限300字內)</small>
+                    </label>
+                    <input type="text" class="form-control" id="describe" placeholder="請描述你的意見/問題">
+                </div>
+                <div class="mb-3">
+                    <label for="formFileLg" class="">
+                        參考圖片
+                        <small>(僅支援PNG、JPG格式，每張5MB內)</small>
+                    </label>
+                    <input class="form-control form-control-lg" id="formFileLg" type="file">
+                    <p>可提供意見/問題截圖(上傳數量 0/3)</p>
+                </div>
+
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" @click="closeModal(false)">Submit</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserFeedback',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        modelType: Boolean
+    },
+    mounted() { },
+    methods: {
+        closeModal(state) {
+            this.$emit('close-modal', state);
+        },
+    },
+
+}
+</script>
+
+<style scoped>
+.feedback-card {
+    width: 70%;
+    height: 80%;
+    margin: auto;
+    background-color: white;
+    position: relative;
+}
+
+.is-required {
+    color: red;
+    font-size: 1rem;
+    margin-left: 5px;
+}
+
+.close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 24px;
+    cursor: pointer;
+    color: #000;
+}
+
+.card-body {
+    text-align: start;
+}
+</style>
